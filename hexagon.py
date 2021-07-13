de089a048f21725eaf08f3331a1d819fafb26344
vonundzuBergen <c.bergen@gmx.net>
2021-07-13 12:19:05 +0200

    Add function to convert coordinates to h3 indices

--- hexagon.py
+++ hexagon.py
@@ -1,6 +1,13 @@
-import h3
+from h3.unstable import vect
+import h3.api.numpy_int as h3
 
 
 def get_hexagon(lat, lng, resolution=8):
     hexagon = h3.geo_to_h3(lat, lng, resolution)
     return hexagon
+
+
+def get_hexagon_vect(lat, lng, res=8):
+    column = vect.geo_to_h3(lat, lng, res)
+    return column
+

f83097285e5f4eb4a8f94df2d9f442e5d543552d
vonundzuBergen <c.bergen@gmx.net>
2021-07-21 15:15:09 +0200

    Added docstrings in hexagon.py

--- hexagon.py
+++ hexagon.py
@@ -8,11 +8,31 @@ import utils
 
 
 def get_hexagon(lat, lng, res=6):
+    """
+        Returns the h3 index for a latitude and longitude at given resolution  .
+        ----------------------------------------------
+        :param
+            lat(int): Latitude.
+            lng(int): Longitude.
+            res(int): Resolution of h3 index.
+        :returns
+            hexagon(int): H3 index.
+    """
     hexagon = h3.geo_to_h3(lat, lng, res)
     return hexagon
 
 
 def get_hexagon_vect(lat, lng, res=6):
+    """
+        Returns the h3 indices for arrays of latitudes and longitudes in given resolution.
+        ----------------------------------------------
+        :param
+            lat(array): Array of latitudes.
+            lng(array): Array of longitudes.
+            res(int): Resolution of h3 index.
+        :returns
+            hexagon(array): Array of h3 indices.
+    """
     column = vect.geo_to_h3(lat, lng, res)
     return column
 
@@ -25,12 +45,25 @@ def polygons_to_hexagon(
         save=False,
         save_name=None,
 ):
+    """
+        This function generates the h3 indices and their boundaries based on given polygons
+        & returns them in a GeoDataFrame.
+        ----------------------------------------------
+        :param
+            gdf(gdp.GeoDataFrame): GeoDataFrame containing polygons as geometry. If None the function
+            reads a GeoJson  from path.
+            res(int): Resolution of h3 index.
+            filename(str): File to read, if no gdf is given.
+            path(str): Path of file to read, if no gdf is given.
+            save(boolean): If True saves the created GeoDataFrame as GeoJson in path.
+            save_name(str): Name of GeoJson.
+        :returns
+            hex_gdf(gdp.GeoDataFrame): GeoDataFrame containing h3 indices and their boundaries.
+    """
     hexagon_index = []
     hexagon_boundaries = []
     if gdf is None:
         gdf = utils.read_geo_dataset(filename=filename, path=path)
-        geometry = gdf["geometry"]
-
     gdf = gdf.explode(column="geometry").reset_index()
     for polygon in gdf.geometry:
         hexagons = h3.polyfill(polygon.__geo_interface__, res=res, geo_json_conformant=True)

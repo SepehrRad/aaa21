be512186ff29369acac69d28ea51537fa55a56aa
vonundzuBergen <c.bergen@gmx.net>
2021-07-21 12:18:33 +0200

    Refactored census_tract_to_hexagon to use df.explode and Chicago city boundaries

--- hexagon.py
+++ hexagon.py
@@ -18,23 +18,22 @@ def get_hexagon_vect(lat, lng, resolution=9):
 
 
 def census_tract_to_hexagon(
-    geometry=None,
-    res=9,
-    filename="Boundaries - Census Tracts - 2010.geojson",
-    path=utils.get_data_path(),
-    save=False,
-    save_name=None,
+        gdf=None,
+        res=9,
+        filename="Boundaries - City.geojson",
+        path=utils.get_data_path(),
+        save=False,
+        save_name=None,
 ):
     hexagon_index = []
     hexagon_boundaries = []
-    if geometry is None:
+    if gdf is None:
         gdf = utils.read_geo_dataset(filename=filename, path=path)
         geometry = gdf["geometry"]
-    for polygon in geometry:
-        polygon = list(polygon)[0]
-        hexagons = h3.polyfill(
-            polygon.__geo_interface__, res=res, geo_json_conformant=True
-        )
+
+    gdf = gdf.explode(column="geometry").reset_index()
+    for polygon in gdf.geometry:
+        hexagons = h3.polyfill(polygon.__geo_interface__, res=res, geo_json_conformant=True)
         for hexagon in hexagons:
             hexagon_index.append(hexagon)
             hexagon_boundaries.append(Polygon(h3.h3_to_geo_boundary(hexagon, True)))
@@ -47,24 +46,4 @@ def census_tract_to_hexagon(
         if save_name is None:
             save_name = f"Hex{res}.geosjon"
         utils.write_geo_dataset(hex_gdf, filename=save_name)
-    return hex_gdf
-
-
-# Slower than census_tract_to_hexagon
-def polygon_to_hexagon(geometry, res=9):
-    hex_gdf = gpd.GeoDataFrame(columns=["hex", "geometry"], crs="EPSG:4326")
-    hex_gdf["geometry"] = geometry
-    hex_gdf["hex"] = hex_gdf.apply(
-        lambda x: h3.polyfill(
-            x["geometry"].__geo_interface__, res=res, geo_json_conformant=True
-        ),
-        axis=1,
-    )
-    hex_gdf.drop(columns=["geometry"], inplace=True)
-    hex_gdf = hex_gdf.explode("hex", ignore_index=True)
-    hex_gdf.dropna(inplace=True)
-    hex_gdf = hex_gdf.drop_duplicates("hex")
-    hex_gdf["geometry"] = hex_gdf.apply(
-        lambda x: Polygon(h3.h3_to_geo_boundary(x["hex"], True)), axis=1
-    )
     return hex_gdf

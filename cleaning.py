9c7fc395228e6492b07ffceb7e07d3c364970457
Simon Wolf <nonrotten@gmail.com>
2021-07-21 14:42:19 +0200

    Update docs in cleaning.py after commit 33f651e5f9bc3e402ffdd7301121e506e3534d23

--- cleaning.py
+++ cleaning.py
@@ -173,7 +173,7 @@ def _merge_additional_columns(df, file="Taxi_Trips.parquet"):
 def _add_cyclical_features(df):
     """
     This function turns the Trip Start/End Timestamp columns into 5 columns each, divided by Month, Day, Hour, Weekday
-    and Is Weekend. Afterwards the Timestamp columns are removed and the processed DataFrame is returned.
+    and Is Weekend.
     ----------------------------------------------
     :param
         df(pd.DataFrame): DataFrame to be processed.

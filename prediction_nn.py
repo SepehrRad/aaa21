43584c6eab890cf598c638551bb1ce551d0d347e
Sepehr Salemirad <Sepehr@gmx.org>
2021-08-04 13:15:21 +0200

    NN Models implemented
    This commit implements all the nn models. Also adds utility functions and functions to produce spatial and temporal models.

--- prediction_nn.py
+++ prediction_nn.py
@@ -6,18 +6,28 @@ def _preprocess_data_for_nn(df, temporal_resolution):
     """
     Doc String!
     """
-    df = add_datepart(df, 'Trip Start Timestamp')
-    df = df.astype({'Trip Start TimestampWeek': 'uint32'})
-    df = df.astype({'Trip Start TimestampElapsed': 'int64'})
+
+    if temporal_resolution == 'D':
+        df = add_datepart(df, 'Trip Start Timestamp', prefix='')
+    else:
+        df = add_datepart(df, 'Trip Start Timestamp', prefix='', time=True)
+        _ = [c for c in df.columns if ('minute' in c.lower() or 'second' in c.lower())]
+        df.drop(_, axis=1, inplace=True)
+    df = df.astype({'Week': 'uint32'})
+    df = df.astype({'Elapsed': 'int64'})
+    # Only Columns with more than 35 distinct values will be considered as continuous
     target = f"Demand ({temporal_resolution})"
-    # Only Columns with more than 12 distinct values will be consider as categories
-    cont_vars, cat_vars = cont_cat_split(df, max_card=12)
+    cont_vars, cat_vars = cont_cat_split(df, max_card=35, dep_var=target)
+    # Spacial temporal columns that should be considered as categories in an embedding structure
+    cont_vars.remove('Dayofyear')
+    cat_vars.append('Dayofyear')
+    cont_vars.remove('Week')
+    cat_vars.append('Week')
     hex_regex = re.compile(".*hex")
     _ = list(filter(hex_regex.match, cont_vars))
     if bool(_):
         [cont_vars.remove(entry) for entry in _]
         cat_vars.extend(_)
-    cont_vars.remove(target)
     return df, target, cont_vars, cat_vars
 
 
@@ -46,12 +56,11 @@ def split_data_sets_for_nn(df, temporal_resolution, test_size=0.2, validation_si
                                    cat_names=cat_vars,
                                    cont_names=cont_vars,
                                    y_names=target,
-                                   batch_size=batch_size,
                                    y_block=RegressionBlock(n_out=1),
                                    splits=val_split)
 
     # Creating the final data loaders
-    dls = train_val_data.dataloaders()
+    dls = train_val_data.dataloaders(batch_size=batch_size)
 
     # Test dl
     # test_dl = TabDataLoader(test_data, bs=batch_size, shuffle=False, drop_last=False)

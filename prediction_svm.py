00ff0e7b07f0253290a1c03d210610c0989f29a3
Simon Wolf <nonrotten@gmail.com>
2021-08-17 17:03:37 +0200

    First grid search

--- prediction_svm.py
+++ prediction_svm.py
@@ -1,6 +1,7 @@
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
 
 
 def split_data_sets_for_svm(df, test_size=0.2):
@@ -31,3 +32,32 @@ def make_pipeline_for_svm(cat_vars, cont_vars, model):
         ('svr model', svr_model)
     ])
     return svr_pipeline
+
+
+def find_best_parameters_for_model(
+    pipeline, X_train, y_train, model_params, scoring, verbose=True):
+    """
+    This function performs a grid search with five time series splits on the training set.
+    ----------------------------------------------
+    :param
+           df (pandas.DataFrame): The given pandas data frame containing data which
+                                  need to be split into train and test data sets.
+           scoring(String): The scoring metric used for grid search
+           model: Used model for prediction.
+           X_train (pandas.DataFrame): Training features
+           y_train (pandas.DataFrame): Target
+           pipeline (sklearn.pipeline): The pipeline with the model and transformers which will be used for grid search.
+    """
+    print(f"Running grid search for the model based on {scoring}")
+    grid_pipeline = GridSearchCV(
+        estimator=pipeline,
+        param_grid=model_params,
+        n_jobs=-1,
+        cv=TimeSeriesSplit(n_splits=5),
+        scoring=scoring,
+        verbose=verbose,
+    )
+    grid_pipeline.fit(X_train, y_train)
+    print(f"Best {scoring} Score was: {grid_pipeline.best_score_}")
+    print(f"The best hyper parameters for the model are:")
+    print(grid_pipeline.best_params_)
